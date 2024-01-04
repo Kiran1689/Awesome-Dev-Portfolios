07e6ad445b5db9b2e4c5235f8d5ae15b61859982
Kiran1689 <kirannaragund197@gmail.com>
2024-01-04 19:52:50 +0530

    install shadcn and tailwind

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import CardList from './components/CardList';
 import CardModal from './components/CardModal'; 
 import Footer from './components/Footer';
 import ScrollToTopButton from './components/ScrollToTopButton';
+import SignUp from './components/SignUp'
 
 const App = () => {
   const [cards, setCards] = useState([]);
@@ -55,6 +56,7 @@ const App = () => {
 
   return (
     <div className="app">
+      <SignUp />
       <Navbar
         searchTerm={searchTerm}
         handleSearchChange={(event) => setSearchTerm(event.target.value)}
