31da52bcca60d867bc0b75e8bafccb3c20bf1710
Karthik S Kumar <sk.karthik315@gmail.com>
2024-03-01 22:30:45 +0530

    feat: added skeleton loaders in portfolios section

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React from "react";
 
-const CardList = ({ cards, onCardClick  }) => (
-  <div className="card-list">
-    {cards.map((card) => (
-      <div className="card-item" onClick={() => onCardClick(card)}>
-        <img className="card-type-icon" src={card.image} alt='screenshot'/>
-        <div className="card-name">{card.name}</div>
-        {/*
+const CardList = ({ cards, onCardClick }) => {
+  const MAX_SKELETON_LOADERS = 9;
+
+  return (
+    <div className="card-list">
+      {cards.length > 0
+        ? cards.map((card, index) => (
+            <div className="card-item" key={index} onClick={() => onCardClick(card)}>
+              <img className="card-type-icon" src={card.image} alt="screenshot" />
+              <div className="card-name">{card.name}</div>
+              {/*
         <div className="card-footer">
           <div className="likes">120 people liked this</div>
           <div className="actions">
@@ -27,10 +31,29 @@ const CardList = ({ cards, onCardClick  }) => (
           </div>
         </div>
          */}
-      </div>
-    
-    ))}
-  </div>
-);
+            </div>
+          ))
+        : Array(MAX_SKELETON_LOADERS)
+            .fill(null)
+            .map((_, index) => (
+              <div className="rounded-lg animate-pulse w-5/6 md:w-full" key={index}>
+                <div className="flex flex-col items-center justify-center w-full h-56 bg-gray-400 rounded">
+                  <svg
+                    className="w-10 h-10 text-gray-200 dark:text-gray-600 my-auto"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 18"
+                  >
+                    <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+                  </svg>
+
+                  <div className="w-11/12 h-7 bg-red-400 mb-2 rounded mt-auto"></div>
+                </div>
+              </div>
+            ))}
+    </div>
+  );
+};
 
 export default CardList;
