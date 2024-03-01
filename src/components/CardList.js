6be6d1ab27b89947299b7e8031ba05a10b7658e3
Karthik S Kumar <sk.karthik315@gmail.com>
2024-03-01 22:33:18 +0530

    fix: changed the text loader color

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -48,7 +48,7 @@ const CardList = ({ cards, onCardClick }) => {
                     <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
                   </svg>
 
-                  <div className="w-11/12 h-7 bg-red-400 mb-2 rounded mt-auto"></div>
+                  <div className="w-11/12 h-7 bg-gray-200 dark:text-gray-600 mb-2 rounded mt-auto"></div>
                 </div>
               </div>
             ))}
